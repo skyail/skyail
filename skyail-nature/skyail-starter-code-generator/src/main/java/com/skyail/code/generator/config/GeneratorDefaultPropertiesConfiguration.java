144c933ef5c6cf24b31807a84ddf5c6b14e349ad
zhouxw <2506009521@qq.com>
2021-01-09 11:20:13 +0800

    替换 oracle 驱动包

---
+++ skyail-nature/skyail-starter-code-generator/src/main/java/com/skyail/code/generator/config/GeneratorDefaultPropertiesConfiguration.java
@@ -0,0 +1,13 @@
+package com.skyail.code.generator.config;
+
+import com.skyail.launch.annotation.SkyailPropertySource;
+
+/**
+ * @Description: TODO
+ * @Auther: zxing
+ * @Date: 2021/1/7 14:03
+ * @company：CTTIC
+ */
+@SkyailPropertySource("classpath:/generator.properties")
+public class GeneratorDefaultPropertiesConfiguration {
+}
