d020fb36a930b15caf1e8644e326a9eedaa1486d
git <2506009521@qq.com>
2021-01-13 22:13:50 +0800

    增加 skyail-ants 基础依赖包

---
+++ skyail-nature/skyail-starter-cloud-common/src/main/java/com/skyail/cloud/common/constant/CommonConstant.java
@@ -0,0 +1,12 @@
+package com.skyail.cloud.common.constant;
+
+public interface CommonConstant {
+
+    String NACOS_SERVER_DEV = "http://localhost:8848";
+
+    String NACOS_SERVER_TEST = "http://localhost:8848";
+
+    String NACOS_SERVER_PROD = "http://121.36.58.81:8848";
+
+    String APPLICATION_NAME = "skyail";
+}
---
+++ skyail-nature/skyail-starter-cloud-common/src/main/java/com/skyail/cloud/common/util/SkyailApplication.java
@@ -0,0 +1,34 @@
+package com.skyail.cloud.common.util;
+
+import com.skyail.cloud.common.constant.CommonConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.util.StringUtils;
+
+import java.util.Properties;
+
+@Slf4j
+public class SkyailApplication {
+
+    public static void run(Class<?> primarySource,String serverName, String... args){
+        Properties properties = System.getProperties();
+        if(StringUtils.isEmpty(serverName)){
+            throw new RuntimeException("请指定服务名称");
+        }
+        properties.setProperty("spring.application.name",serverName);
+
+        String env = properties.getProperty("spring.profiles.active");
+        env = StringUtils.isEmpty(env)?"dev":env;
+        log.info("["+env+"]启动---");
+        if( "dev".equals(env)){
+            properties.setProperty("spring.cloud.nacos.discovery.server-addr", CommonConstant.NACOS_SERVER_DEV);
+        }else if("test".equals(env)){
+            properties.setProperty("spring.cloud.nacos.discovery.server-addr", CommonConstant.NACOS_SERVER_TEST);
+        }else if("prod".equals(env)){
+            properties.setProperty("spring.cloud.nacos.discovery.server-addr", CommonConstant.NACOS_SERVER_PROD);
+        }
+
+        SpringApplication.run(primarySource , args);
+    }
+
+}
